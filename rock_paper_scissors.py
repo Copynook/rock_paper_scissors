5b3b6df23a122089915a8fc6b0aa95382303684c
Vasi_lena <105110262+Copynook@users.noreply.github.com>
2022-10-13 22:20:33 +0300

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -6,9 +6,10 @@ computer_wins = 0
 options = ["rock", "paper", "scissors"]
 
 while True:
-    user_input = input("Type Rock/Paper/Scissors or 'Q' to quit.").lower()
+    user_input = input("Type Rock/Paper/Scissors or 'Q' to quit. ").lower()
     if user_input == "q":
         break
+
     if user_input not in options:
         continue
 
@@ -20,15 +21,19 @@ while True:
     if user_input == "rock" and computer_pick == "scissors":
         print("You won!")
         user_wins += 1
+    
     elif user_input == "scissors" and computer_pick == "paper":
         print("You won!")
         user_wins += 1
+    
     elif user_input == "paper" and computer_pick == "rock":
         print("You won!")
         user_wins += 1
+    
     else:
         print("You lost!")
         computer_wins += 1
 
 print("You won", user_wins, "times.")
 print("The computer won", computer_wins, "times.")
+print("Goodbye!")
